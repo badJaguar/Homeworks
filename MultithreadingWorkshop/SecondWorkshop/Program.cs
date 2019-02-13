c07e0c6e4051fcbd80874aaf5a4c9494fbae21bb
Alexander Ataakgayev <bad.bad.jaguar@gmail.com>
2019-02-13 15:46:01 +0300

    Add Second workshop task

---
+++ MultithreadingWorkshop/SecondWorkshop/Program.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using System;
+using System.Collections.ObjectModel;
+
+namespace SecondWorkshop
+{
+    class Program
+    {
+        static void Main()
+        {
+            var bogus = new Person();
+            var words = bogus.Random.WordsArray(10);
+
+            Console.WriteLine(CollectionFiller.Add(words));
+            Console.ReadKey();
+        }
+    }
+
+    public static class CollectionFiller
+    {
+        public static Collection<string> Add(string[] word) => new Collection<string>
+            {
+                string.Join(" ", word)
+            };
+    }
+}
--- MultithreadingWorkshop/TasksWorkshop/Program.cs
+++ MultithreadingWorkshop/TasksWorkshop/Program.cs
@@ -8,37 +8,35 @@ namespace MultithreadingWorkshop
     {
         static void Main()
         {
-            TasksChain t = new TasksChain();
             var randomArray = new Random();
             var randomNum = new Random();
             var bytes = new byte[10];
             randomArray.NextBytes(bytes);
 
-            var arr = t.DisplayArray(bytes).Result;
-            var rand = randomNum.Next(1,9);
+            var arr = TasksChain.DisplayArray(bytes).Result;
+            var rand = randomNum.Next(1, 9);
             Console.WriteLine($"Multiplier is {rand}");
-            
+
             Console.WriteLine();
 
-            var t1 = Task.Run(()=> Console.WriteLine(arr));
-            var t2 = Task.Run(()=> Console.WriteLine(t.ArrayMultiplier(arr, rand).Result));
-            var t3 = Task.Run(()=> Console.WriteLine(t.Ordered(t.ArrayMultiplier(arr, rand).Result).Result));
-            var t4 = Task.Run(()=> Console.WriteLine(t.AvgResult(t.Ordered(t.ArrayMultiplier(arr, rand)
+            var t1 = Task.Run(() => Console.WriteLine(arr));
+            var t2 = Task.Run(() => Console.WriteLine(TasksChain.ArrayMultiplierAsync(arr, rand).Result));
+            var t3 = Task.Run(() => Console.WriteLine(TasksChain.OrderedAsync(TasksChain.ArrayMultiplierAsync(arr, rand).Result).Result));
+            var t4 = Task.Run(() => Console.WriteLine(TasksChain.AvgResultAsync(TasksChain.OrderedAsync(TasksChain.ArrayMultiplierAsync(arr, rand)
                 .Result).Result).Result));
             Console.ReadKey();
         }
     }
 
-    public class TasksChain
+    public static class TasksChain
     {
-        
-        public Task<string> DisplayArray(byte[] numbers) =>
+        public static Task<string> DisplayArray(byte[] numbers) =>
 
              Task.FromResult(string.Join(" ", (
                 from n in numbers
                 select n).AsParallel()));
 
-        public Task<string> ArrayMultiplier(string numbers, int num) =>
+        public static Task<string> ArrayMultiplierAsync(string numbers, int num) =>
 
              Task.FromResult(string.Join(" ", (
                 from n in numbers.Split()
@@ -46,16 +44,16 @@ namespace MultithreadingWorkshop
                 select parsed * num).ToArray()))
                .ContinueWith(task => task.Result);
 
-        public Task<string> Ordered(string numbers) =>
+        public static Task<string> OrderedAsync(string numbers) =>
 
-        Task.FromResult(string.Join(" ", 
+        Task.FromResult(string.Join(" ",
                     from n in numbers.Split()
                     let parsed = int.Parse(n)
                     orderby parsed descending
                     select parsed))
-                   .ContinueWith(s=>s.Result);
+                   .ContinueWith(s => s.Result);
 
-        public async Task<string> AvgResult(string numbers) =>
+        public static async Task<string> AvgResultAsync(string numbers) =>
 
         await Task.FromResult(string.Join(" ",
                 (from n in numbers.Split()
