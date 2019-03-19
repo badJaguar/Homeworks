393bde0d8b66381534229007a20cbac801322b40
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-03-19 20:44:50 +0300

    ParserHw Add

---
+++ ParserHw/Parser.BL/Parser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+
+namespace Parser.BL
+{
+    public class Parser
+    {
+        public async Task<List<string>> GetRefs(string url)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(url);
+            var elements = document.QuerySelectorAll("a")
+                .OfType<IHtmlAnchorElement>();
+
+            var values = (
+                from element in elements
+                let attribute = element.GetAttribute("href")
+                where attribute != "/" && !(attribute is null)
+                select element.Href).Distinct().ToList();
+            
+            return values;
+        }
+
+        public async Task ParseHtml()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync("https://metanit.com");
+
+            using (FileStream fs = 
+                File.Create(@"C:\Users\aleks\Desktop\New folder\some.html", 1024))
+            {
+                var info = new UTF8Encoding(true)
+                    .GetBytes(document.ToHtml());
+
+                fs.Write(info, 0, info.Length);
+            }
+        }
+    }
+}
---
+++ ParserHw/ParserHw/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+
+namespace ParserHw
+{
+    class Program
+    {
+        static void Main()
+        {
+            var parser = new Parser.BL.Parser();
+            //Console.Write("Paste a URL: ");
+
+            //var url = Console.ReadLine();
+            var refs = Task.Run(async () => await parser.GetRefs("https://metanit.com"));
+            foreach (var c in refs.Result)
+            {
+                Console.WriteLine(c);
+            }
+            parser.ParseHtml();
+            Console.ReadKey();
+        }
+
+       
+    }
+}
