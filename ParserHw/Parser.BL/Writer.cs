ff012a29c64ac5a446c2dba6a76c801226998588
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-03-19 23:10:01 +0300

    add Writer class

--- ParserHw/Parser.BL/Writer.cs
+++ ParserHw/Parser.BL/Writer.cs
@@ -1,7 +1,20 @@
-﻿namespace Parser.BL
+﻿using System.IO;
+using System.Text;
+
+namespace Parser.BL
 {
     public class Writer
     {
-        
+        //public void HtmlWriter()
+        //{
+        //    using (FileStream fs =
+        //        File.Create($"C:\\Users\\aleks\\Desktop\\New folder\\{element.ClassName}.html", 1024))
+        //    {
+        //        var info = new UTF8Encoding(true)
+        //            .GetBytes(document.ToHtml());
+
+        //        fs.Write(info, 0, info.Length);
+        //    }
+        //}
     }
 }
